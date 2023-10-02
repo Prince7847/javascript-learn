2526889475326af3fe4addd3aa3e614b6c90c519
PRINCE DAHALIYA <112964223+Prince7847@users.noreply.github.com>
2023-10-02 06:49:30 +0000

    done

---
+++ 01_basic/01_var.js
@@ -0,0 +1,12 @@
+const name ='prince';
+let lastName = 'dahaliya';
+var age = '25';
+city = 'greater noida';
+
+// name = 'pinsu'  // not allowed becoz can't change
+
+console.table([name, lastName, age, city]);   // use .table to print mutliple var in one time.
+
+
+// we dont prefer to use var
+// beacause of issue in block scope and functional scope
